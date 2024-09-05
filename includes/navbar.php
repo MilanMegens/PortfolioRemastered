e72387c937d22c00b3c9c7136f75ca0146b28281
MiwanMegons <milanmegens@gmail.com>
2024-09-05 18:14:51 +0200

    De start

---
+++ includes/navbar.php
@@ -0,0 +1,23 @@
+<body>
+    <div class="navbar">
+        <div class="brand">{Milan Megens}</div>
+        <div class="hamburger" onclick="toggleMenu()">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+        <div class="desktop-menu" id="desktop-menu">
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Services</a>
+            <a href="#">Contact</a>
+        </div>
+    </div>
+
+    <div class="menu" id="menu">
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Services</a>
+        <a href="#">Contact</a>
+    </div>
+</body>
---
+++ index.php
@@ -0,0 +1,26 @@
+<!--
+███╗░░░███╗██╗██╗░░░░░░█████╗░███╗░░██╗░█████╗░░██████╗░██████╗
+████╗░████║██║██║░░░░░██╔══██╗████╗░██║██╔══██╗██╔════╝██╔════╝
+██╔████╔██║██║██║░░░░░███████║██╔██╗██║██║░░██║╚█████╗░╚█████╗░
+██║╚██╔╝██║██║██║░░░░░██╔══██║██║╚████║██║░░██║░╚═══██╗░╚═══██╗
+██║░╚═╝░██║██║███████╗██║░░██║██║░╚███║╚█████╔╝██████╔╝██████╔╝
+╚═╝░░░░░╚═╝╚═╝╚══════╝╚═╝░░╚═╝╚═╝░░╚══╝░╚════╝░╚═════╝░╚═════╝░
+-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio Milan Megens</title>
+    <!-- CSS Links -->
+    <link rel="stylesheet" href="styles/main.css">
+
+    <!-- JavaScript Links -->
+    <script src="scripts/main.js" defer></script>
+</head>
+<body>
+    <!-- Include van de Navbar -->
+    <?php include('includes/navbar.php') ?>
+
+</body>
+</html>
