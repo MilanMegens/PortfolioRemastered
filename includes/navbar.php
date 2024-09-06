2ba757e9488ca257ea2755f9b461818ad6a5781d
MiwanMegons <milanmegens@gmail.com>
2024-09-06 20:14:48 +0200

    06-09-2024

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,4 +1,4 @@
-<body>
+<div class="navigatiebalk">  
     <div class="navbar">
         <div class="brand">{Milan Megens}</div>
         <div class="hamburger" onclick="toggleMenu()">
@@ -15,9 +15,41 @@
     </div>
 
     <div class="menu" id="menu">
-        <a href="#">Home</a>
-        <a href="#">About</a>
-        <a href="#">Services</a>
-        <a href="#">Contact</a>
+        <!-- Kruisje -->
+        <div class="kruis" onclick="toggleMenu()">
+            <div class="diagonal left"></div>
+            <div class="diagonal right"></div>
+        </div>
+
+        <!-- Navigatie titel -->
+        <p class="baarle">Navigatie</p>
+        <hr class="lijntje-1">
+
+        <!-- Navigatie items -->
+        <div class="nav-items">
+            <a href="#">Over mij</a>
+            <a href="#">Vaardigheden</a>
+            <a href="#">Projecten</a>
+            <a href="#">Contact</a>
+        </div>
+
+        <!-- Socials -->
+        <p class="elraab">Socials</p>
+        <hr class="lijntje-1">
+
+        <!-- Icons -->
+        <div class="socials">
+            <a href="www.linkedin.com/in/milanmegens2007" target="_blank">
+                <img src="content/icons/linkedin.png">
+            </a>
+            <a href="https://github.com/MiwanMegons" target="_blank">
+                <img src="content/icons/github.png">
+            </a>
+            <a href="https://www.instagram.com/milanmegens/" target="_blank">
+                <img src="content/icons/instagram.png">
+            </a>
+            <a href="mailto:milanoss2007@gmail.com" target="_blank">
+                <img src="content/icons/mail.png">
+            </a>
+        </div>
     </div>
-</body>
--- index.php
+++ index.php
@@ -14,13 +14,52 @@
     <title>Portfolio Milan Megens</title>
     <!-- CSS Links -->
     <link rel="stylesheet" href="styles/main.css">
+    <link rel="stylesheet" href="styles/navbar.css">
 
     <!-- JavaScript Links -->
     <script src="scripts/main.js" defer></script>
+    <script src="scripts/navbar.js" defer></script>
 </head>
 <body>
     <!-- Include van de Navbar -->
     <?php include('includes/navbar.php') ?>
+
+    <!-- Homepage -->
+    <div class="homepage">
+        <!-- Functie HomePage -->
+        <p class="functie">- Software Developer</p>
+
+        <!-- Titel HomePage -->
+        <h1 class="titel">Front & back-end developer</h1>
+
+        <!-- Beschrijving HomePage -->
+        <p class="beschrijving">Welkom op mijn website! Hier kun
+            je meer lezen over mijn ervaringen
+            en projecten. Voel je vrij om contact
+            met me op te nemen!
+        </p>
+
+        <div class="scrolling-tekst">
+            <div class="horizontal-scrolling-items">
+                <div class="horizontal-scrolling-items__item">
+                Milan Megens - Milan Megens - Milan Megens - Milan Megens - Milan Megens - Milan Megens - Milan Megens - Milan Megens -Milan Megens - Milan Megens - Milan Megens - Milan Megens -Milan Megens - Milan Megens - Milan Megens - Milan Megens -Milan Megens - Milan Megens - Milan Megens - Milan Megens - &nbsp
+                </div>
+
+                <div class="horizontal-scrolling-items__item">
+                Milan Megens - Milan Megens - Milan Megens - Milan Megens - Milan Megens - Milan Megens - Milan Megens - Milan Megens -Milan Megens - Milan Megens - Milan Megens - Milan Megens -Milan Megens - Milan Megens - Milan Megens - Milan Megens -Milan Megens - Milan Megens - Milan Megens - Milan Megens - &nbsp
+                </div>
+            </div>
+        </div>
+        <!-- Scroll tekst en foto -->
+        <img class="foto-1" src="content/fotos/milan-1.png" alt="">
+
+        
+    </div>
+
+    <!-- Over mij pagina -->
+     <div class="overmij">
+        
+     </div>
 
 </body>
 </html>
