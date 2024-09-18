3717d3ff4ad7276755c824f29ee791cb5046071b
MilanMegensAppmatic <milan.megens@appmatic.nl>
2024-09-18 11:07:24 +0200

    18-09-2024

--- paginas/contact.php
+++ paginas/contact.php
@@ -1,188 +1,192 @@
 <div class="contact">
-    <h1>Contact</h1>
+    <div class="contactblokjes">
+        <h1>Contact</h1>
 
-    <!-- Mail box -->
-    <div class="box" id="contact">
-        <div class="tekst">
-            <p>Je kan me hier op mailen</p>
-            <h2>milanoss2007@gmail.com</h2>
-        </div>
-
-        <div class="pijl">
-            <a href="mailto:milanoss2007@gmail.com"><i class="ph-light ph-caret-circle-right"></i></a>
-        </div>
-    </div>
-
-    <!-- Linkedin box -->
-    <div class="box">
-        <div class="tekst">
-            <p>Stuur een berichtje op linkedin</p>
-            <h2>milanmegens2007</h2>
-        </div>
-
-        <div class="pijl">
-            <a href="https://www.linkedin.com/in/milanmegens2007" target="_blank"><i class="ph-light ph-caret-circle-right"></i></a>
-        </div>
-    </div>
-
-    <!-- Instagram box -->
-    <div class="box">
-        <div class="tekst">
-            <p>Volg me op instagram</p>
-            <h2>milanmegens</h2>
-        </div>
-
-        <div class="pijl">
-            <a href="https://www.instagram.com/milanmegens/" target="_blank"><i class="ph-light ph-caret-circle-right"></i></a>
-        </div>
-    </div>
-
-    <!-- Contact form box -->
-    <div class="box">
-        <div class="tekst">
-            <p>Of open het</p>
-            <h2>Contact formulier</h2>
-        </div>
-
-        <div class="pijl">
-        <a href="#" id="openOverlay"><i class="ph-light ph-caret-circle-right"></i></a>
-        </div>
-    </div>
-
-    <!-- Overlay -->
-    <div id="overlay" class="overlay">
-        <div class="overlay-content">
+        <!-- Mail box -->
+        <div class="box" id="contact">
             <div class="tekst">
-                <h1>Contactformulier</h1>
-                <a href="#" id="closeOverlay"><i class="ph-bold ph-x"></i></a>
+                <p>Je kan me hier op mailen</p>
+                <h2>milanoss2007@gmail.com</h2>
             </div>
-            <form action="includes/send-mail.php" method="POST">
-                <input type="text" name="voornaam" placeholder="Voornaam *" required>
-                <input type="text" name="achternaam" placeholder="Achternaam *" required>
 
-                <div class="contact-info">
-                    <input type="email" name="email" placeholder="Email *" required>
-                    <input type="tel" name="telefoon" placeholder="Telefoonnummer (Optioneel)">
+            <div class="pijl">
+                <a href="mailto:milanoss2007@gmail.com"><i class="ph-light ph-caret-circle-right"></i></a>
+            </div>
+        </div>
+
+        <!-- Linkedin box -->
+        <div class="box">
+            <div class="tekst">
+                <p>Stuur een berichtje op linkedin</p>
+                <h2>milanmegens2007</h2>
+            </div>
+
+            <div class="pijl">
+                <a href="https://www.linkedin.com/in/milanmegens2007" target="_blank"><i class="ph-light ph-caret-circle-right"></i></a>
+            </div>
+        </div>
+
+        <!-- Instagram box -->
+        <div class="box">
+            <div class="tekst">
+                <p>Volg me op instagram</p>
+                <h2>milanmegens</h2>
+            </div>
+
+            <div class="pijl">
+                <a href="https://www.instagram.com/milanmegens/" target="_blank"><i class="ph-light ph-caret-circle-right"></i></a>
+            </div>
+        </div>
+
+        <!-- Contact form box -->
+        <div class="box">
+            <div class="tekst">
+                <p>Of open het</p>
+                <h2>Contact formulier</h2>
+            </div>
+
+            <div class="pijl">
+            <a href="#" id="openOverlay"><i class="ph-light ph-caret-circle-right"></i></a>
+            </div>
+        </div>
+
+        <!-- Overlay -->
+        <div id="overlay" class="overlay">
+            <div class="overlay-content">
+                <div class="tekst">
+                    <h1>Contactformulier</h1>
+                    <a href="#" id="closeOverlay"><i class="ph-bold ph-x"></i></a>
                 </div>
+                <form action="includes/send-mail.php" method="POST">
+                    <input type="text" name="voornaam" placeholder="Voornaam *" required>
+                    <input type="text" name="achternaam" placeholder="Achternaam *" required>
 
-                <textarea name="bericht" rows="4" placeholder="Bericht *" required></textarea>
+                    <div class="contact-info">
+                        <input type="email" name="email" placeholder="Email *" required>
+                        <input type="tel" name="telefoon" placeholder="Telefoonnummer (Optioneel)">
+                    </div>
 
-                <button type="submit">Verzenden</button>
-            </form>
+                    <textarea name="bericht" rows="4" placeholder="Bericht *" required></textarea>
+
+                    <button type="submit">Verzenden</button>
+                </form>
+            </div>
         </div>
     </div>
 
-    <!-- Titel en knopjes -->
-    <div class="iets">
-        <div class="stroop">
-            <h1>Referenties</h1>
-        </div>
-        <div class="aroow">
-            <i class="ph-bold ph-caret-left" id="prev"></i>
-            <i class="ph-bold ph-caret-right" id="next"></i>
-        </div>
-    </div>
-
-    <div class="reviews" id="reviews">
-        <!-- Review Ian -->
-        <div class="boxke" id="review-0">
-            <div class="top">
-                <div class="titel">
-                    <h2>Ian Robeerst</h2>
-                    <h3>Docent programmeren MBO</h3>
-                </div>
-                <div class="icon">
-                    <i class="ph-fill ph-quotes"></i>
-                </div>
+    <div class="referenties">
+        <!-- Titel en knopjes -->
+        <div class="iets">
+            <div class="stroop">
+                <h1>Referenties</h1>
             </div>
-            <p>
-                Milan is een gemotiveerde student die graag nieuwe technieken leert
-                en direct toepast in het maken van diverse, humoristische interactieve
-                projecten. Hij is een serieuze en enthousiaste leerling die altijd een
-                positieve bijdrage levert in de klas. Zijn creativiteit en 
-                doorzettingsvermogen maken hem een waardevolle en inspirerende medestudent.
-            </p>
+            <div class="aroow">
+                <i class="ph-bold ph-caret-left" id="prev"></i>
+                <i class="ph-bold ph-caret-right" id="next"></i>
+            </div>
         </div>
 
-        <!-- Review Ingrid -->
-        <div class="boxke" id="review-1">
-            <div class="top">
-                <div class="titel">
-                    <h2>Ingrid Verhaegh</h2>
-                    <h3>Docent Nederlands MBO</h3>
-                </div>
-                <div class="icon">
-                    <i class="ph-fill ph-quotes"></i>
-                </div>
+        <div class="reviews" id="reviews">
+            <!-- Review Ian -->
+            <div class="boxke" id="review-0">
+                <div class="top">
+                    <div class="titel">
+                        <h2>Ian Robeerst</h2>
+                        <h3>Docent programmeren MBO</h3>
+                    </div>
+                    <div class="icon">
+                        <i class="ph-fill ph-quotes"></i>
+                    </div>
                 </div>
                 <p>
-                    Milan is een serieuze, sociale, gedreven en enthousiaste student.   
-                    Als hij iets niet begrijpt, stelt hij vragen. Hij is altijd bereid
-                    om iemand te helpen, waardoor hij goed in de groep ligt.
+                    Milan is een gemotiveerde student die graag nieuwe technieken leert
+                    en direct toepast in het maken van diverse, humoristische interactieve
+                    projecten. Hij is een serieuze en enthousiaste leerling die altijd een
+                    positieve bijdrage levert in de klas. Zijn creativiteit en 
+                    doorzettingsvermogen maken hem een waardevolle en inspirerende medestudent.
                 </p>
             </div>
 
-        <!-- Review Janneke -->
-        <div class="boxke" id="review-2">
-            <div class="top">
-                <div class="titel">
-                    <h2>Janneke Peters</h2>
-                    <h3>Docent Engels MBO</h3>
+            <!-- Review Ingrid -->
+            <div class="boxke" id="review-1">
+                <div class="top">
+                    <div class="titel">
+                        <h2>Ingrid Verhaegh</h2>
+                        <h3>Docent Nederlands MBO</h3>
+                    </div>
+                    <div class="icon">
+                        <i class="ph-fill ph-quotes"></i>
+                    </div>
+                    </div>
+                    <p>
+                        Milan is een serieuze, sociale, gedreven en enthousiaste student.   
+                        Als hij iets niet begrijpt, stelt hij vragen. Hij is altijd bereid
+                        om iemand te helpen, waardoor hij goed in de groep ligt.
+                    </p>
                 </div>
-                <div class="icon">
-                    <i class="ph-fill ph-quotes"></i>
-                </div>
-                </div>
-                <p>
-                    Milan is een fijne student om in de klas te hebben. Tijdens mijn
-                    lessen was hij een gangmaker, maar kon hij ook serieus aan
-                    het werk wanneer dat moest. Dit maakte het heel erg leuk en
-                    gezellig om hem bij de lessen te hebben. Daarbij oefende hij
-                    ook altijd zijn Engels in de lessen door met mij in gesprek te
-                    gaan. Milan is dus een leuke toevoeging aan een team of groep.
-                </p>
-            </div>    
-        
 
-        <!-- Review Levje <3 -->
-        <div class="boxke" id="review-3">
-            <div class="top">
-                <div class="titel">
-                    <h2>Lev Posthumus</h2>
-                    <h3>Vriend / Klasgenoot MBO</h3>
-                </div>
-                <div class="icon">
-                    <i class="ph-fill ph-quotes"></i>
-                </div>
-                </div>
-                <p>
-                    Ik werk al een jaar samen met Milan en hij is echt
-                    een kanjer. Hij kan goed samenwerken en heeft altijd
-                    zijn aandeel op tijd af, maar als jij dat niet hebt
-                    helpt hij je geweldig! Naast school is hij ook echt
-                    een goede jongen die er altijd voor je is.
-                </p>
-        </div>
-        
-        <!-- Review Nick -->
-        <div class="boxke" id="review-4">
-            <div class="top">
-                <div class="titel">
-                    <h2>Nick Kluijtmans</h2>
-                    <h3>Vriend / Klasgenoot MBO</h3>
-                </div>
-                <div class="icon">
-                    <i class="ph-fill ph-quotes"></i>
-                </div>
-                </div>
-                <p>
-                    Milan is een aardige jongen. hij is heel behulpzaam en heel
-                    gemotiveerd. hij heeft me vaak uitgelegd hoe ik sommige dingen
-                    moet maken. verder is hij een hele leuke jongen en gelukkig
-                    houd hij ook wel van een grapje
-                </p>
+            <!-- Review Janneke -->
+            <div class="boxke" id="review-2">
+                <div class="top">
+                    <div class="titel">
+                        <h2>Janneke Peters</h2>
+                        <h3>Docent Engels MBO</h3>
+                    </div>
+                    <div class="icon">
+                        <i class="ph-fill ph-quotes"></i>
+                    </div>
+                    </div>
+                    <p>
+                        Milan is een fijne student om in de klas te hebben. Tijdens mijn
+                        lessen was hij een gangmaker, maar kon hij ook serieus aan
+                        het werk wanneer dat moest. Dit maakte het heel erg leuk en
+                        gezellig om hem bij de lessen te hebben. Daarbij oefende hij
+                        ook altijd zijn Engels in de lessen door met mij in gesprek te
+                        gaan. Milan is dus een leuke toevoeging aan een team of groep.
+                    </p>
+                </div>    
+            
+
+            <!-- Review Levje <3 -->
+            <div class="boxke" id="review-3">
+                <div class="top">
+                    <div class="titel">
+                        <h2>Lev Posthumus</h2>
+                        <h3>Vriend / Klasgenoot MBO</h3>
+                    </div>
+                    <div class="icon">
+                        <i class="ph-fill ph-quotes"></i>
+                    </div>
+                    </div>
+                    <p>
+                        Ik werk al een jaar samen met Milan en hij is echt
+                        een kanjer. Hij kan goed samenwerken en heeft altijd
+                        zijn aandeel op tijd af, maar als jij dat niet hebt
+                        helpt hij je geweldig! Naast school is hij ook echt
+                        een goede jongen die er altijd voor je is.
+                    </p>
             </div>
-        </div>
+            
+            <!-- Review Nick -->
+            <div class="boxke" id="review-4">
+                <div class="top">
+                    <div class="titel">
+                        <h2>Nick Kluijtmans</h2>
+                        <h3>Vriend / Klasgenoot MBO</h3>
+                    </div>
+                    <div class="icon">
+                        <i class="ph-fill ph-quotes"></i>
+                    </div>
+                    </div>
+                    <p>
+                        Milan is een aardige jongen. hij is heel behulpzaam en heel
+                        gemotiveerd. hij heeft me vaak uitgelegd hoe ik sommige dingen
+                        moet maken. verder is hij een hele leuke jongen en gelukkig
+                        houd hij ook wel van een grapje
+                    </p>
+                </div>
+            </div>
+        </div><!-- Reviews moeten in deze div -->   
         <img class="wow" src="content/fotos/milan-3.png" alt="Foto onderaan">
-    </div><!-- Reviews moeten in deze div -->    
+    </div> 
 </div>
